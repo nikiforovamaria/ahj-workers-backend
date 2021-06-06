b21ad67f0c1a99b1d8b73206fdabc92a238473df
Maria Nikiforova <nikiforovamary@gmail.com>
2021-06-06 20:48:11 +0600

    third commit

--- src/js/server.js
+++ src/js/server.js
@@ -1,29 +1,31 @@
-const http = require("http");
-const Koa = require("koa");
+const http = require('http');
+const Koa = require('koa');
+const koaBody = require('koa-body')
 const slow = require('koa-slow');
 const { v4: uuidv4 } = require('uuid');
 const Router = require("koa-router");
+const cors = require('@koa/cors');
 const faker = require('faker');
 
 const app = new Koa();
 
 const data = {
-  "status": "ok",
-  "timestamp": `${ new Date().toLocaleDateString() } ${ new Date().toLocaleTimeString() }`,
-  "articles": [
-    {
-      "id": uuidv4(),
-      "image": faker.image.avatar(),
-      "description": faker.lorem.words(10),
-      "received": `${ new Date().toLocaleDateString() } ${ new Date().toLocaleTimeString() }`
-    },
-    {
-      "id": uuidv4(),
-      "image": faker.image.avatar(),
-      "description": faker.lorem.words(10),
-      "received": `${ new Date().toLocaleDateString() } ${ new Date().toLocaleTimeString() }`
-    },
-  ]
+    "status": "ok",
+    "timestamp": `${ new Date().toLocaleDateString() } ${ new Date().toLocaleTimeString() }`,
+    "articles": [
+      {
+        "id": uuidv4(),
+        "image": faker.image.avatar(),
+        "description": faker.lorem.words(10) ,
+        "received": `${ new Date().toLocaleDateString() } ${ new Date().toLocaleTimeString() }`,
+      },
+      {
+        "id": uuidv4(),
+        "image": faker.image.avatar(),
+        "description": faker.lorem.words(10),
+        "received": `${ new Date().toLocaleDateString() } ${ new Date().toLocaleTimeString() }`,
+      },
+    ]
 }
 
 console.log(data);
@@ -32,42 +34,20 @@ app.use(slow({
   delay: 4000,
 }));
 
-app.use(async (ctx, next) => {
-  const origin = ctx.request.get("Origin");
-  if (!origin) {
-    return await next();
-  }
+app.use(koaBody({
+  urlencoded: true,
+  multipart: true,
+  json: true,
+}));
 
-  const headers = { "Access-Control-Allow-Origin": "*" };
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    'Access-Control-Allow-Origin': true,
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }));
 
-  if (ctx.request.method !== "OPTIONS") {
-    ctx.response.set({ ...headers });
-    try {
-      return await next();
-    } catch (e) {
-      e.headers = { ...e.headers, ...headers };
-      throw e;
-    }
-  }
-
-  if (ctx.request.get("Access-Control-Request-Method")) {
-    ctx.response.set({
-      ...headers,
-      "Access-Control-Allow-Methods": "GET, POST, PUD, DELETE, PATCH",
-    });
-  }
-
-  if (ctx.request.get("Access-Control-Request-Headers")) {
-    ctx.response.set(
-      "Access-Control-Allow-Headers",
-      ctx.request.get("Access-Control-Request-Headers")
-    );
-  }
-
-  ctx.response.status = 204;
-
-  ctx.respond = false;
-});
+const router = new Router();
 
 app.use(async (ctx) => {
   switch (ctx.request.url) {
@@ -77,9 +57,8 @@ app.use(async (ctx) => {
   }
 });
 
-const router = new Router();
-
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 7070;
-const server = http.createServer(app.callback()).listen(port);
+const server = http.createServer(app.callback())
+server.listen( port , () => console.log('server started'));
